9b4e9903c28ed425d9eef38f6ecfd628e0f33a94
Tarskan <55380090+Tarskan@users.noreply.github.com>
2021-12-08 21:23:38 +0100

    Amélioration du jeu de données

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
-INSERT INTO creneau VALUES (1, 'Matin', 'test rezqqsfdgsdcsfd', '2015-12-17');
-INSERT INTO creneau VALUES (2, 'Midi', 'test rezqqsfdgsdcsfd', '2015-12-17');
-INSERT INTO creneau VALUES (3, 'Gouter', 'test rezqqsfdgsdcsfd', '2015-12-17');
-INSERT INTO creneau VALUES (4, 'Soir', 'test rezqqsfdgsdcsfd', '2015-12-17');
+INSERT INTO creneau VALUES (1, 'Matin', '8h-11h', '2015-12-17');
+INSERT INTO creneau VALUES (2, 'Midi', '12h-14h', '2015-12-17');
+INSERT INTO creneau VALUES (3, 'Gouter', '16h-18h', '2015-12-17');
+INSERT INTO creneau VALUES (4, 'Soir', '19h-21h', '2015-12-17');
 
-INSERT INTO position VALUES (1, 'Positiontest', 'test rezqqsfdgsdcsfd', 59.561535614, 70.6513615);
-INSERT INTO position VALUES (2, 'secondtest', 'test rezqqsfdgsdcsfd', 129.561535614, 12.6513615);
+INSERT INTO position VALUES (1, 'Positiontest', 'Vente de hotdog à la sortie du métro', 59.561535614, 70.6513615);
+INSERT INTO position VALUES (2, 'secondtest', 'Cuisine asiatique près de la FGES', 129.561535614, 12.6513615);
 
-INSERT INTO truck VALUES (1, 'Trucktest', 'test rezqqsfdgsdcsfd', 1, 1);
+INSERT INTO truck VALUES (1, 'Camion a hotdog', 'A un coup sur la porte droite', 1, 2);
+INSERT INTO truck VALUES (2, 'Camion de cuisine Thai', 'Une révision devras être bientot faite', 2, 2);
